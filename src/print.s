759de07120049313fc53a30af4a0515e5fd2babd
Burak Güner <burak.guner@callstack.com>
2024-11-23 16:59:42 +0300

    we can corrupt x16

--- src/print.s
+++ src/print.s
@@ -6,8 +6,6 @@ _print:
   mov x2, x1 ; len
   mov x1, x0 ; start addr
   mov x0, #1 ; file descriptor = stdout
-  mov x4, x16 ; persist x16
   mov x16, #4 ; write syscall code
   svc #0x80 ; call supervisor
-  mov x16, x4 ; restore x16
   ret

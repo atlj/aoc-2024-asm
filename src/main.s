8b160b182c430220f774a4c276965c26b61ab108
Burak Güner <burak.guner@callstack.com>
2024-11-24 04:42:21 +0300

    dynamic length

--- src/main.s
+++ src/main.s
@@ -2,14 +2,21 @@
 .global _main
 .align 4
 
-.equ BUFFER_LENGTH, 16
+.equ BUFFER_LENGTH, 255
 
 _main:
   read_stdin mem@page, BUFFER_LENGTH
-  print mem@page, BUFFER_LENGTH
+  mov x19, x0
+
+  cmp x0, #0
+  mov x0, #1
+  b.lt custom_exit
+
+  print mem@page, x19
 
 exit:
   mov x0, 0 ; status
+custom_exit:
   mov x16, 1 ; exit
   svc #0x80
 

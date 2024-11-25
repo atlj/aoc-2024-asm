befecbaa1426b135b7f820315045978b0a610e1b
Burak Güner <burak.guner@callstack.com>
2024-11-25 16:13:42 +0300

    have an input and output big enough

--- src/main.s
+++ src/main.s
@@ -2,17 +2,13 @@
 .global _main
 .align 4
 
-.equ BUFFER_LENGTH, 255
+.equ BUFFER_LENGTH, 0x100000
+.equ END_OF_FILE, 0
 
 _main:
-  read_stdin mem@page, BUFFER_LENGTH
-  mov x19, x0
-
-  cmp x0, #0
-  mov x0, #1
-  b.lt custom_exit
-
-  print mem@page, x19
+read_again:
+  read_stdin input@page, BUFFER_LENGTH
+  print output@page, x0
 
 exit:
   mov x0, 0 ; status
@@ -21,6 +17,7 @@ custom_exit:
   svc #0x80
 
 .data
-mem: .fill BUFFER_LENGTH + 1, 1, 0
+input: .fill BUFFER_LENGTH + 1, 1, 0
+output: .fill BUFFER_LENGTH + 1, 1, 0
 hellostr: .ascii "hello mom!\n"
 infile: .ascii "hello6.txt"
